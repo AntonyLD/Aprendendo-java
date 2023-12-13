85d28cc9ec301e620bd2717a818ae16720578aba
AntonyLD <antonyldcamargo@gmail.com>
2023-12-13 14:16:42 -0300

    .

---
+++ aula-11/ex-009.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log(`Vivendo no ${pais}`)
+if (pais == 'Brasil', 'brasil'){
+    console.log('Você é Brasileiro')
+} 
+else{
+    console.log('Você é Estrangeiro')
+}
---
+++ aula-11/exe-8.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`a velocidade do seu carro é ${vel} km`)
+if (vel > 60){
+    console.log(`voce ta rapido de mais safado, toma essa multa`)
+}
+console.log(`Dirija sempre usando sinto`)
